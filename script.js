1428a67c2a1827471dcd6e4a51368fefb1ad9c5c
AlyssonSouza <alyssonvictor2002@gmail.com>
2023-09-25 15:49:15 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 let nome = prompt("Digite o nome do heroi: ")
 let xp = prompt("Digite a quantidade de xp do heroi: ")
 let elo = ""
+
+switch (xp) {
+    case xp <= 1000:
+        elo = "Ferro"
+        break;
+
+    case xp <= 2000:
+        elo = "Bronze";
+        break;
+
+    case xp <= 5000:
+        elo = "Prata";
+        break;
+    
+    case xp <= 7000:
+        elo = "Ouro"
+        break;
+    
+    case xp <= 8000:
+        elo = "Platina"
+        break;
+
+    case xp <= 9000:
+        elo = "Ascendente"
+        break;
+    
+    case xp <= 10000:
+        elo = "Imortal"
+        break;
+
+
+    default:
+        elo = "Radiante"
+        break;
+}
+
 console.log(`O Herói de nome ${nome} está no nível de ${elo}`);
